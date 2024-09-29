c3617b080cf467f02bb1f5ec3fda8dfba61c33d6
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-09-29 23:27:51 +0900

    tuple

---
+++ pkg/rust-concept/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // shadowing
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    println!("The value of x is: {}", x);
+
+    // tuple type
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+    println!("The value of y is: {}", tup.1);
+}
