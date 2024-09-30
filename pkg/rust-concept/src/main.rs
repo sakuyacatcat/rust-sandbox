2ebcc555728634f9749dc20bf20c1f59c780efa9
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-09-30 10:01:02 +0900

    prefix underscore

--- pkg/rust-concept/src/main.rs
+++ pkg/rust-concept/src/main.rs
@@ -11,14 +11,14 @@ fn main() {
 
     // tuple type
     let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (x, y, z) = tup;
+    let (_x, y, _z) = tup;
     println!("The value of y is: {}", y);
     println!("The value of y is: {}", tup.1);
 
     // list type
     let a: [i32; 5] = [1,2,3,4,5];
-    let first = a[0];
-    let last = a[a.len() - 1];
-    let b = [3; 5];
-    let months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+    let _first = a[0];
+    let _last = a[a.len() - 1];
+    let _b = [3; 5];
+    let _months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
 }
