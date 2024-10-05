2f8048b34a4250ea182a2d677ab3253a65a04554
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-10-06 00:43:00 +0900

    ownership

--- pkg/rust-concept/src/main.rs
+++ pkg/rust-concept/src/main.rs
@@ -103,9 +103,9 @@ fn main() {
     }
 
     // ownership
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{}", s);
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}, world", s2);
 }
 
 fn type_of<T>(_: &T) -> &str {
