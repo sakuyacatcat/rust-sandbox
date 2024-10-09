145647b809a6035f2738f61cdfbe4dba33be9f4a
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-10-09 23:30:06 +0900

    implement Debug trait

--- pkg/rust-struct/src/main.rs
+++ pkg/rust-struct/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
@@ -6,6 +7,7 @@ struct User {
 }
 
 fn main() {
+    // struct
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
@@ -13,12 +15,24 @@ fn main() {
         sign_in_count: 1,
     };
     user1.email = String::from("anotheremail@example.com");
+    println!("{:?}", user1);
 
     let user2 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
         ..user1
     };
+    println!("{:?}", user2);
+
+    // struct with tupple
+    #[derive(Debug)]
+    struct Color(i32, i32, i32);
+    #[derive(Debug)]
+    struct Point(i32, i32, i32);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    println!("{:#?}", black);
+    println!("{:#?}", origin);
 }
 
 fn build_user(email: String, username: String) -> User {
