88b025329b7dae0c7cd5b2f22e060260b8535cee
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-10-11 00:09:18 +0900

    enum with method

--- pkg/rust-enum/src/main.rs
+++ pkg/rust-enum/src/main.rs
@@ -1,3 +1,41 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:#?}", self);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // enum
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+    route(home);
+    route(loopback);
+
+    // enum with method
+    let q = Message::Quit;
+    q.call();
+    let m = Message::Move { x: 1, y: 2 };
+    m.call();
+    let w = Message::Write(String::from("hello"));
+    w.call();
+    let c = Message::ChangeColor(1, 2, 3);
+    c.call();
+}
+
+fn route(ip: IpAddr) {
+    println!("{:#?}", ip);
 }
