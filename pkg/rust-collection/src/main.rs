c15d09d365d2ed152bddcb9606a3b96a2257ee6f
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-10-12 17:51:43 +0900

    rust collection

---
+++ pkg/rust-collection/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    // Vector
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1,2,3];
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let v = vec![1,2,3,4,5];
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element")
+    }
+
+    // Iterating over the values in a vector
+    let v = vec![1,2,3,4,5];
+    let does_not_exist = v.get(100);
+
+    // vector iteration
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    // vector and enums
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    println!("{:?}", row);
+
+    // string and &str
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
+    let mut s = String::from("lo");
+    s.push_str("l");
+    println!("{}", s);
+
+    // string and ownership
+    let s1 = String::from("hello ");
+    let s2 = String::from("world");
+    let s3 = s1 + &s2;
+    let s4 = format!("{}-{}", s3, s2);
+    println!("{}", s4);
+}
