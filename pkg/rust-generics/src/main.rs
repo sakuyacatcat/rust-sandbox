b4fc7ad4e5f99608199421dcdc891da0526906fa
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-10-15 00:07:54 +0900

    generics and trait

---
+++ pkg/rust-generics/src/main.rs
@@ -0,0 +1,57 @@
+mod lib;
+
+use lib::{Summary, NewsArticle, Tweet};
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    // // generic function
+    // let number_list = vec![34, 50, 25, 100, 65];
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
+
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
+
+    // generic struct
+    let p1 = Point { x: 5, y: 10 };
+    let p2 = Point { x: 1.0, y: 4.0 };
+    let p3 = p1.mixup(p2);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            // もちろん、ご存知かもしれませんがね、みなさん
+            "of course, as you probably already know, people",
+        ),
+        reply: false.to_string(),
+        retweet: false.to_string(),
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
