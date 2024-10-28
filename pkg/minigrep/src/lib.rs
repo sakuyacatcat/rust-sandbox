ac0a4017b0c7f267be8b0c1b9aa5e1339d3db43f
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-10-28 22:30:38 +0900

    minigrep test

--- pkg/minigrep/src/lib.rs
+++ pkg/minigrep/src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -28,4 +29,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("With text:\n{}", contents);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_new_with_two_args() {
+        let args = vec![String::from("minigrep"), String::from("query"), String::from("filename")];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.query, "query");
+        assert_eq!(config.filename, "filename");
+    }
+
+    #[test]
+    fn config_new_with_one_arg() {
+        let args = vec![String::from("minigrep"), String::from("query")];
+        let result = Config::new(&args);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "not enough arguments");
+    }
+
+    #[test]
+    fn config_new_with_no_args() {
+        let args = vec![String::from("minigrep")];
+        let result = Config::new(&args);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "not enough arguments");
+    }
 }
