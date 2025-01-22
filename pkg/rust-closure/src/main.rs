0ff9850703816ed4fd06c2c9d4f8ff430a4dc6dc
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-22 23:54:47 +0900

    update

--- pkg/rust-closure/src/main.rs
+++ pkg/rust-closure/src/main.rs
@@ -1,7 +1,7 @@
-use std::thread;
-use std::time::Duration;
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let simulated_user_specified_value = 10;
@@ -13,48 +13,114 @@ fn main() {
     );
 }
 
-struct Cache<T, K, V>
+// struct Cache<T, K, V>
+// where
+//     T: Fn(K) -> V,
+//     K: Eq + Hash + Copy,
+// {
+//     calculation: T,
+//     value: HashMap<K, V>,
+// }
+
+// impl<T, K, V> Cache<T, K, V>
+// where
+//     T: Fn(K) -> V,
+//     K: Eq + Hash + Copy,
+//     V: Clone,
+// {
+//     fn new(calculation: T) -> Cache<T, K, V> {
+//         Cache {
+//             calculation
+//             value: HashMap::new(),
+//         }
+//     }
+
+//     fn value(&mut self, arg: K) -> V {
+//         self.value.entry((arg).or_insert_with(|| (self.calculation)(arg))).clone()
+//     }
+// }
+
+struct Cacher<F, A, R>
 where
-    T: Fn(K) -> V,
-    K: Eq + Hash + Copy,
+    F: Fn(A) -> R,
+    A: Eq + Hash + Copy,
+    R: Clone,
 {
-    calculation: T,
-    value: HashMap<K, V>,
+    calculation: F,
+    values: Option<A, R>,
 }
 
-impl<T, K, V> Cache<T, K, V>
+impl<F, A, R> Cacher<F, A, R>
 where
-    T: Fn(K) -> V,
-    K: Eq + Hash + Copy,
-    V: Clone,
+    F: Fn(A) -> R,
+    A: Eq + Hash + Clone,
+    R: Clone,
 {
-    fn new(calculation: T) -> Cache<T, K, V> {
-        Cache {
-            calculation
-            value: HashMap::new(),
+    fn new(calculation: F) -> Cacher<F, A, R> {
+        Cacher {
+            calculation,
+            values: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: K) -> V {
-        self.value.entry((arg).or_insert_with(|| (self.calculation)(arg))).clone()
+    fn value(&mut self, arg: A) -> R {
+        match self.values.get(&arg) {
+            Some(v) => v.clone(),
+            None => {
+                let result = (self.calculation)(arg.clone());
+                self.values.insert(arg.clone(), result.clone());
+                result
+            },
+        }
     }
 }
 
+// struct Cacher<T>
+// where
+//     T: Fn(u32) -> u32
+// {
+//     calculation: T,
+//     value: Option<u32>,
+// }
+
+// impl<T> Cacher<T>
+//     where T: Fn(u32) -> u32
+// {
+//     fn new(calculation: T) -> Cacher<T> {
+//         Cacher {
+//             calculation,
+//             value: None,
+//         }
+//     }
+
+//     fn value(&mut self, arg: u32) -> u32 {
+//         match self.value {
+//             Some(v) => v,
+//             None => {
+//                 let v = (self.calculation)(arg);
+//                 self.value = Some(v);
+//                 v
+//             },
+//         }
+//     }
+// }
+
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_closure = Cache::new(|num| {
+    let mut expensive_closure = Cacher::new(|num: &u32| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
-        num
+        *num
     });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure.value(intensity)
+            expensive_closure.value(&intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure.value(intensity)
+            expensive_closure.value(&intensity)
         );
     } else {
         if random_number == 3 {
@@ -62,7 +128,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure.value(intensity)
+                expensive_closure.value(&intensity)
             );
         }
     }
@@ -70,10 +136,11 @@ fn generate_workout(intensity: u32, random_number: u32) {
 
 #[test]
 fn call_with_different_values() {
-    let mut c = Cache::new(|a| a);
+    let mut c = Cache::new(|a: &u32| *a);
 
     let v1 = c.value(1);
     let v2 = c.value(2);
 
+    assert_eq!(v1, 1);
     assert_eq!(v2, 2);
 }
