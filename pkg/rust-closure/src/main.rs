462543b8418d8eb6a9cdfb7eec49ddbab172d3eb
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-25 21:19:09 +0900

    add struct cacher for memorization of closure result

--- pkg/rust-closure/src/main.rs
+++ pkg/rust-closure/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::hash::Hash;
 use std::thread;
 use std::time::Duration;
 
@@ -13,114 +12,51 @@ fn main() {
     );
 }
 
-// struct Cache<T, K, V>
-// where
-//     T: Fn(K) -> V,
-//     K: Eq + Hash + Copy,
-// {
-//     calculation: T,
-//     value: HashMap<K, V>,
-// }
-
-// impl<T, K, V> Cache<T, K, V>
-// where
-//     T: Fn(K) -> V,
-//     K: Eq + Hash + Copy,
-//     V: Clone,
-// {
-//     fn new(calculation: T) -> Cache<T, K, V> {
-//         Cache {
-//             calculation
-//             value: HashMap::new(),
-//         }
-//     }
-
-//     fn value(&mut self, arg: K) -> V {
-//         self.value.entry((arg).or_insert_with(|| (self.calculation)(arg))).clone()
-//     }
-// }
-
-struct Cacher<F, A, R>
+struct Cacher<T>
 where
-    F: Fn(A) -> R,
-    A: Eq + Hash + Copy,
-    R: Clone,
+    T: Fn(u32) -> u32,
 {
-    calculation: F,
-    values: Option<A, R>,
+    calculation: T,
+    value: HashMap<u32, u32>,
 }
 
-impl<F, A, R> Cacher<F, A, R>
-where
-    F: Fn(A) -> R,
-    A: Eq + Hash + Clone,
-    R: Clone,
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
 {
-    fn new(calculation: F) -> Cacher<F, A, R> {
+    fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            values: HashMap::new(),
+            value: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: A) -> R {
-        match self.values.get(&arg) {
-            Some(v) => v.clone(),
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value.get(&arg) {
+            Some(&v) => v,
             None => {
-                let result = (self.calculation)(arg.clone());
-                self.values.insert(arg.clone(), result.clone());
+                let result = (self.calculation)(arg);
+                self.value.insert(arg, result);
                 result
-            },
+            }
         }
     }
 }
 
-// struct Cacher<T>
-// where
-//     T: Fn(u32) -> u32
-// {
-//     calculation: T,
-//     value: Option<u32>,
-// }
-
-// impl<T> Cacher<T>
-//     where T: Fn(u32) -> u32
-// {
-//     fn new(calculation: T) -> Cacher<T> {
-//         Cacher {
-//             calculation,
-//             value: None,
-//         }
-//     }
-
-//     fn value(&mut self, arg: u32) -> u32 {
-//         match self.value {
-//             Some(v) => v,
-//             None => {
-//                 let v = (self.calculation)(arg);
-//                 self.value = Some(v);
-//                 v
-//             },
-//         }
-//     }
-// }
-
-
 fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_closure = Cacher::new(|num: &u32| {
+    let mut expensive_closure = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
-        *num
+        num
     });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure.value(&intensity)
+            expensive_closure.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure.value(&intensity)
+            expensive_closure.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -128,7 +64,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure.value(&intensity)
+                expensive_closure.value(intensity)
             );
         }
     }
@@ -136,11 +72,13 @@ fn generate_workout(intensity: u32, random_number: u32) {
 
 #[test]
 fn call_with_different_values() {
-    let mut c = Cache::new(|a: &u32| *a);
+    let mut c = Cacher::new(|a| a);
 
     let v1 = c.value(1);
     let v2 = c.value(2);
+    let v3 = c.value(1);
 
     assert_eq!(v1, 1);
     assert_eq!(v2, 2);
+    assert_eq!(v3, 1);
 }
