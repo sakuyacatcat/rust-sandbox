dc3287cc867c8af67000038aa9a5da88476630ef
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-30 01:22:43 +0900

    iterator next method utilization

--- pkg/rust-closure/src/main.rs
+++ pkg/rust-closure/src/main.rs
@@ -104,3 +104,15 @@ fn call_with_different_values() {
     assert_eq!(v2, 2);
     assert_eq!(v3, 1);
 }
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
