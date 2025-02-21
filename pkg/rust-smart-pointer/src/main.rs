b3779954e733178542749973601e603b5dfe7bfc
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-02-21 23:50:30 +0900

    Refactor List to use Rc instead of Box and implement MyBox and CustomSmartPointer

--- pkg/rust-smart-pointer/src/main.rs
+++ pkg/rust-smart-pointer/src/main.rs
@@ -1,13 +1,79 @@
+#[derive(Debug)]
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use List::{Cons, Nil};
+use std::rc::Rc;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
 
 fn main() {
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    // let list = Cons(1,
+    //     Box::new(Cons(2,
+    //         Box::new(Cons(3,
+    //             Box::new(Nil))))));
+    // println!("{:?}", list);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
+
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+        println!("c: {:?}", c);
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    println!("b: {:?}", b);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
