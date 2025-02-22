9188848a8373dfbcda3060dc7c2dd82e467a1d38
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-02-22 23:08:40 +0900

    Implement Mutex and Arc for shared state management in multi-threading

--- pkg/rust-concurrency/src/main.rs
+++ pkg/rust-concurrency/src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -78,4 +79,27 @@ fn main() {
     for received in rx {
         println!("Got: {}", received);
     }
+
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {:?}", m);
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
 }
