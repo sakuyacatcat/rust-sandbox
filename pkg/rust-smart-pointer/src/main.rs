d11988466ef74724e8977575dc1e0f892e3ad41f
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-02-22 22:04:41 +0900

    Implement LimitTracker with Messenger trait and add tests for warning messages

--- pkg/rust-smart-pointer/src/main.rs
+++ pkg/rust-smart-pointer/src/main.rs
@@ -3,13 +3,20 @@
 //     Cons(i32, Box<List>),
 //     Nil,
 // }
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
+
+
 use List::{Cons, Nil};
 use std::rc::Rc;
+use std::cell::RefCell;
 
 struct MyBox<T>(T);
 
@@ -61,17 +68,26 @@ fn main() {
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-        println!("c: {:?}", c);
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
-    println!("b: {:?}", b);
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    //     println!("c: {:?}", c);
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // println!("b: {:?}", b);
+
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    *value.borrow_mut() += 10;
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
 
 fn hello(name: &str) {
