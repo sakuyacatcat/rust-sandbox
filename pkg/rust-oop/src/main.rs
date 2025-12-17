a0394264403e3676d0c828ea679ac5e55e1f9986
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-12-17 23:15:13 +0900

    Add AveragedCollection and Screen structures with Draw trait implementation

--- pkg/rust-oop/src/main.rs
+++ pkg/rust-oop/src/main.rs
@@ -1,3 +1,30 @@
+// extern crate gui;
+// use gui::Draw;
+
+// struct SelectBox {
+//     width: u32,
+//     height: u32,
+//     options: Vec<String>,
+// }
+
+// impl Draw for SelectBox {
+//     fn draw(&self) {
+//         // セレクトボックスの描画ロジック
+//         println!("Drawing a select box with options: {:?}", self.options);
+//     }
+// }
+
+use blog::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
