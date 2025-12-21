531aa8a729836b3e02b4927d6705d5e1ba71d89a
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-12-21 23:15:19 +0900

    Add rstest for unit testing in echor package

--- pkg/echor/src/lib.rs
+++ pkg/echor/src/lib.rs
@@ -23,3 +23,19 @@ pub fn run(text: Vec<String>, omit_newline: bool) -> String {
     let ending = if omit_newline { "" } else { "\n" };
     format!("{}{}", text.join(" "), ending)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec!["Hello,", "world!"], false, "Hello, world!\n")]
+    #[case(vec!["Hello,", "world!"], true, "Hello, world!")]
+    #[case(vec!["This", "is", "a", "test."], false, "This is a test.\n")]
+    fn test_run(#[case] input: Vec<&str>, #[case] omit_newline: bool, #[case] expected: &str) {
+        let input_strings: Vec<String> = input.iter().map(|s| s.to_string()).collect();
+        let result = run(input_strings, omit_newline);
+        assert_eq!(result, expected);
+    }
+}
