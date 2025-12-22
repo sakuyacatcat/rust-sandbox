821ae2fdd8498ca9e54e2f80319600197183aa44
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-12-23 00:08:45 +0900

    テスト関数の戻り値型を修正し、Result型を削除

--- pkg/echor/tests/cli.rs
+++ pkg/echor/tests/cli.rs
@@ -1,43 +1,39 @@
 use assert_cmd::cargo::cargo_bin_cmd;
 use predicates::prelude::*;
 
-type TestResult = Result<(), Box<dyn std::error::Error>>;
-
 #[test]
-fn dies_no_args() -> TestResult {
+fn dies_no_args() {
     cargo_bin_cmd!("echor")
         .assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
-    Ok(())
 }
 
-fn runs(args: &[&str], expected: &str) -> TestResult {
+fn runs(args: &[&str], expected: &str) {
     cargo_bin_cmd!("echor")
         .args(args)
         .assert()
         .success()
         .stdout(predicate::eq(expected));
-    Ok(())
 }
 
 
 #[test]
-fn hello1() -> TestResult {
+fn hello1() {
     runs(&["Hello, world!"], "Hello, world!\n")
 }
 
 #[test]
-fn hello2() -> TestResult {
+fn hello2() {
     runs(&["Hello,", "world!"], "Hello, world!\n")
 }
 
 #[test]
-fn hello1_no_newline() -> TestResult {
+fn hello1_no_newline() {
     runs(&["-n", "Hello, world!"], "Hello, world!")
 }
 
 #[test]
-fn hello2_no_newline() -> TestResult {
+fn hello2_no_newline() {
     runs(&["-n", "Hello,", "world!"], "Hello, world!")
 }
