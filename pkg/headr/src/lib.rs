6ff8589518291eceb8fd9ba86f887a26dcd37537
sakuyacatcat <shingo.takagi0116@gmail.com>
2026-01-19 23:35:44 +0900

    headrパッケージの引数解析機能を実装し、エラーハンドリングを追加

---
+++ pkg/headr/src/lib.rs
@@ -0,0 +1,87 @@
+use clap::{Command, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("headr")
+        .version("0.1.0")
+        .author("ss <shingo.takagi.j7a@gmail.com>")
+        .about("Rust headr")
+        .arg(
+            Arg::new("lines")
+                .short('n')
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10")
+                .value_parser(clap::value_parser!(usize)),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .value_name("BYTES")
+                .conflicts_with("lines")
+                .help("Number of bytes")
+                .value_parser(clap::value_parser!(usize)),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input files")
+                .num_args(0..)
+                .default_value("-"),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches
+            .get_many::<String>("files")
+            .unwrap()
+            .map(|s| s.to_string())
+            .collect(),
+        lines: matches
+            .get_one::<String>("lines")
+            .unwrap()
+            .parse()
+            .unwrap(),
+        bytes: matches
+            .get_one::<String>("bytes")
+            .map(|s| s.parse().unwrap()),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    print!("Config: {:?}\n", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
