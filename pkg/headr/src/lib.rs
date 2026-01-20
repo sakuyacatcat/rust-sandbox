d9abd2896b0cdc9eda0b97ea5b928af3491437cc
sakuyacatcat <shingo.takagi0116@gmail.com>
2026-01-20 09:31:39 +0900

    run関数にバイト数を指定して読み込む機能を追加し、行数指定の読み込み処理を修正

--- pkg/headr/src/lib.rs
+++ pkg/headr/src/lib.rs
@@ -1,7 +1,7 @@
 use clap::{Arg, Command};
 use std::error::Error;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -61,11 +61,27 @@ pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(file) => {
-                for line in file.lines().take(config.lines) {
-                    println!("{}", line?);
+            Ok(mut file) => {
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes as u64);
+                    let mut buffer = vec![0; num_bytes];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!(
+                        "{}",
+                        String::from_utf8_lossy(&buffer[..bytes_read])
+                    );
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    }
                 }
-            }
+           }
         }
     }
     Ok(())
